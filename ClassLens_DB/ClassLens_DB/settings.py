9914095629f0cce9b68a305d9f8b1a316f30da00
Dhriti-5 <gandhi.dhriti2005@gmail.com>
2026-05-15 09:33:22 +0530

    Update settings and requirements

--- ClassLens_DB/ClassLens_DB/settings.py
+++ ClassLens_DB/ClassLens_DB/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x3zih9=91am0f(gkv&16y+n%7i1b91e-^mh&v*_r=_kz@y7t04"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
