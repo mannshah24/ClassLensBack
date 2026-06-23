d486af50c005d4fd2df977666ac47f430e5a8dd1
Mann Shah <mannshah2405@gmail.com>
2026-06-23 19:42:06 +0530

    feat: GPU acceleration and performance optimizations for face processing

--- ClassLens_DB/start_server.bat
+++ ClassLens_DB/start_server.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\Users\Admin\Desktop\ClassLens\ClassLens_DB
 call C:\Users\Admin\Desktop\ClassLens\venv\Scripts\activate.bat
-waitress-serve --threads=32 --host=127.0.0.1 --port=11020 ClassLens_DB.wsgi:application
+waitress-serve --threads=32 --host=127.0.0.1 --port=8000 ClassLens_DB.wsgi:application
 pause
